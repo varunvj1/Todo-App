7a6be2a1c83c1e0c725749291763d4b305bc9d12
Varun Jain <varunvj181@gmail.com>
2021-09-24 15:19:33 +0530

    Added context files

---
+++ src/context/reducer.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { ADD_TODO, REMOVE_TODO } from "./action.types";
+
+export default (state, action) => {
+    switch (action.type) {
+        case ADD_TODO:
+            return [...state, action.payload];
+        case REMOVE_TODO:
+            return state.filter(todo => todo.id !== action.payload);
+
+        default:
+            return state;
+    }
+};
